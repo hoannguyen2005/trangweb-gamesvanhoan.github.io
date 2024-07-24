7703cd4de85e112aacc1016293d035d8c0a21971
hoannguyen2005 <161012480+hoannguyen2005@users.noreply.github.com>
2024-07-24 16:09:06 +0700

    Add files via upload

---
+++ bai1.js
@@ -0,0 +1,83 @@
+// let list = [ 1,7, 5,9,2,6];
+  // for (let i =0 ;i< list.length ;i++){
+  //   if (list[i] %2==0){
+  //     sum = list[i] + list[i+1];
+  //     console.log("Tổng các phần tử chẵn trong mảng là :" + sum);
+  //   }
+  // }
+
+
+//  // In ra tổng các phần tử lớn hơn 5 trong mảng\
+//  let arr = [ 1,7, 5,9,2,6];
+//   let sum =  0;
+//   for (let i =0 ;i< arr.length ;i++){
+//     if (arr[i]>5){
+//       sum = sum + arr[i];
+//       console.log ("tỔNG các phần tử lớn hơn 5 :" + sum);
+//     }
+
+//   }
+
+  // // In ra tổng các phần tử chia 7 dư 2 trong mảng
+  // let arr = [ 1,7, 5,9,2,6];
+  // let sum = 0;
+  // for (let i =0 ;i< arr.length ;i++){
+  //   if (arr[i] %7 ==2){
+  //     sum = sum + arr[i];
+  //     console.log("Tổng các phần tử chia 7 dư 2 là :" + sum);
+  //   }
+  // }
+  
+  // // In ra max của các phần tử chẵn mảng
+  // let arr = [ 1,7, 5,9,2,6];
+  // let max = arr[0];
+  // for (let i =0 ;i< arr.length ;i++){
+  //   if (arr[i]%2==0){
+  //     if (arr[i]>max){
+  //       max = arr[i];
+  //       console.log("max của các phần tử chẵn mảng là "+ max );
+  //     }
+  //   }
+  // }
+  // // In ra lớn nhất của mảng
+  // let arr = [ 1,7, 5,9,2,6];
+  // let max = arr[0];
+  // for (let i = 0 ; i<arr.length;i++ ){
+  //   if(arr[i]>max){
+  //     max = arr[i];
+  //     console.log(" số lơn nhất cảu mảng " + max);
+  //   }
+  // }
+
+  //- In ra số lượng các phần tử chia hết cho 5 trong mảng
+  // let arr = [ 1,7, 5,9,2,6];
+  // let count = 0;
+  // for ( let i  =0; i< arr.length;i++){
+  //   if (arr[i]%5==0){
+  //     count = count +1;
+  //     console.log(" số lượng các phần tử chia hết cho 5 :" + count);
+  //   }
+  // }
+
+//  In ra trung bình các phần tử chia hết cho 5 trong mảng
+// let arr = [ 1,7, 5,9,2,6];
+// let sum = 0;
+// let count = 0;
+// for ( let i  =0; i< arr.length;i++){
+//   if (arr[i]%5==0){
+//     sum = sum + arr[i];
+//     count =count +1;
+//     console.log("trung bình các phần tử chia cho 5" + sum/count);
+//   }
+// }
+
+// // - Cho người dùng nhập vào 1 số và in ra số lần xuất hiện của số đó trong mảng.
+// let arr = [ 1,7, 5,9,2,6];
+// let count =0;
+// let num = Number(prompt("nhập vào 1 số"));
+// for ( let i  =0; i< arr.length;i++){
+//   if (arr[i]==num){
+//     count = count +1;
+//     console.log("số lần xuất hiện của số " + num +"là :"+count);
+//   }
+// }
